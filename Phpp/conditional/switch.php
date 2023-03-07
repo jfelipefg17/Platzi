751117e7f883c3ba9814ab09a1454d174f13b065
jfelipefg17 <jfelipefg17@gmail.com>
2023-03-07 12:21:28 -0500

    switch

---
+++ Phpp/conditional/switch.php
@@ -0,0 +1,26 @@
+/**
+* *this is use just for easy decision 
+*/
+
+<?php
+
+$perro = 7;
+
+switch ($perro) {
+  case 1:
+    echo "su numero favorito es 10";
+    break;
+
+  case 2:
+    echo "su numero favorito es 8";
+    break;
+
+  case 3:
+    echo "su numero favorito es 3";
+    break;
+
+  default:
+    echo "su numero favorito es 7";
+}
+echo "\n";
+?>
