1e1bd3e9e3359907251604110c79e8c88d2e3d07
jfelipefg17 <jfelipefg17@gmail.com>
2023-03-13 09:04:47 -0500

    more php

--- Phpp/functions/return.php
+++ Phpp/functions/return.php
@@ -2,11 +2,30 @@
 
 function felipe() {
 
-  switch(){
+  $random = rand(1, 3);
+  $frase ="";
+
+  switch($random){
     case 1:
-      $f
+      $frase = "Sobe cabeza. \n";
+      break;
+
+    case 2:
+      $frase = "Chao con pan. \n";
+      break;
+
+    case 3:
+      $frase = "A no chao. \n";
+      break;
+
+
+    }
+
+return $frase;
+
   }
-}
+
+echo felipe();
 
 echo "\n";
 ?>
